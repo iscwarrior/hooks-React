7f5812691cd47b5dfcd2c9bce666c90b01af8788
daniel.flores <dani.fguerrero@gmail.com>
2023-06-29 21:52:17 -0600

    add: up practica

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+// interface Props {
+//     text: string;
+// }
+
+// export default Button (props: Props) {
+//     return <button onClick={() => alert("clicked")}> {props.text}</button>;
+// }
+
+export default Button;
